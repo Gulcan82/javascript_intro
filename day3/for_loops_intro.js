49a648b252a24c57054c59f006428f5119d2bb19
Gülcan Kamat <gulcankamat@gmail.com>
2024-03-19 13:00:03 +0100

    Adding file color_dropdown and file loops_intro.

---
+++ day3/for_loops_intro.js
@@ -0,0 +1,38 @@
+// Zeigt volle Schleife von i 1-5
+/*const numbersArray = [1, 2, 3, 4, 5];
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+    
+}
+
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8];
+// Auflisten von 1-8
+for (let i = 0; i < numbersArray.length; i++) {
+    console.log(numbersArray[i]);
+    
+}
+// Auflisten umdrehen von 8 zu 1
+for (let i =numbersArray.length - 1; i >= 0; i--){
+    console.log(numbersArray[i]);
+
+
+    
+}
+*/
+
+// Multiplikation
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8];
+
+for (let i = 0; i < numbersArray.length; i++) {
+    console.log(numbersArray[i]*2);
+    
+}
+
+for (let i =numbersArray.length - 1; i >= 0; i--){
+    console.log(numbersArray[i]*3);
+
+
+    
+}
